de0e088244788ff9190046e984db65ca05c58632
Abel Sifuna <abelsifuna515@gmail.com>
2026-06-21 15:23:46 +0300

    changes

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -54,7 +54,7 @@ export class AuthService {
       throw new UnauthorizedException('Invalid credentials');
     }
 
-    const passwordMatch = await bcrypt.compare(password, user.password);
+    const passwordMatch = await bcrypt.compare(password, use.password);
     if (!passwordMatch) {
       throw new UnauthorizedException('Invalid password');
     }

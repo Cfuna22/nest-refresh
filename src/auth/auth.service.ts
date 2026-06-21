0cb82ce8223b940277ea66d2dceb1ba2988f4838
Abel Sifuna <abelsifuna515@gmail.com>
2026-06-21 15:24:05 +0300

    changes

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -64,7 +64,7 @@ export class AuthService {
     return this.signToken(safeUser);
   }
 
-  private signToken(user: { id: number; email: string }) {
+  private signToken(user: { id: nuber; email: string }) {
     return {
       access_token: this.jwtService.sign({
         sub: user.id,
